9e8d1031b9d093baea10a9f2ea930a2d2758963d
pdroid908 <p1998nr@gmail.com>
2026-08-01 21:19:55 +0700

    p

--- api/index.go
+++ api/index.go
@@ -9,27 +9,20 @@ import (
 var app *gin.Engine
 
 func init() {
-	// Inisialisasi Gin hanya sekali saat cold start
 	gin.SetMode(gin.ReleaseMode)
 	app = gin.New()
 	app.Use(gin.Recovery())
 
-	// Define Route/Endpoint Gin kamu di sini
-	app.GET("/", func(c *gin.Context) {
+	app.GET("/api/v1/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Halo dari Gin + Vercel!",
-			"status":  "success",
-		})
-	})
-
-	app.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"system":  "WorkforceHub API",
+			"status":  "healthy",
+			"message": "Enterprise Attendance System API Ready",
 		})
 	})
 }
 
-// Handler adalah fungsi wajib yang diekspor untuk dibaca oleh Vercel
+// ⚠️ NAMA FUNGSI HARUS 'Handler' (H Kapital) dan MENGGUNAKAN 'http.ResponseWriter', '*http.Request'
 func Handler(w http.ResponseWriter, r *http.Request) {
 	app.ServeHTTP(w, r)
 }
